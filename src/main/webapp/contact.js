5b972aeef2b52c74c4d03e59ec5457ce583d59d7
Prasanth S <91864574+prasanths53@users.noreply.github.com>
2021-12-17 15:28:09 +0530

    Add files via upload

---
+++ src/main/webapp/contact.js
@@ -0,0 +1,17 @@
+const contacts=[];
+
+function saveOrUpdate(){
+    console.log("SaveOrUpdate")
+}
+
+function save(){
+    console.log("Save")
+}
+
+function update(){
+    console.log("update")
+}
+
+function remove(){
+    console.log("remove")
+}
